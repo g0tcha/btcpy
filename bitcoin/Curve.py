d504e95e5e46db43246472b8fbb0234ba56e4e53
Vincent Grossier <vincent.grossier@gmail.com>
2025-08-15 21:19:31 -0400

    Curve class added

---
+++ bitcoin/Curve.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class Curve:
+    """
+    Elliptic Curve over the field of integers modulo a prime.
+    Points on the curve satisfy y^2 = x^3 + a*x + b (mod p).
+    """
+    p: int
+    a: int
+    b: int
+
+# secp256k1 uses a = 0, b = 7, so the function is y^2 = x^3 + 7 (mod p)
+bitcoin_curve = Curve(
+    p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
+    a = 0x0000000000000000000000000000000000000000000000000000000000000000,
+    b = 0x0000000000000000000000000000000000000000000000000000000000000007
+)
