28f1218984c9a5d8c233d651fdc51d992a8209c7
Vincent Grossier <vincent.grossier@gmail.com>
2025-08-24 19:27:50 -0400

    Files renamed + secret key generation

--- bitcoin/generator.py
+++ bitcoin/generator.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from Point import Point
-from Point import G
+from .point import Point
+from .point import G
 
 @dataclass
 class Generator:
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from bitcoin import Curve
-from bitcoin import Point
+from bitcoin.curve import Curve
+from bitcoin.curve import bitcoin_curve
+from bitcoin.point import Point
+from bitcoin.generator import Generator
+from bitcoin.generator import bitcoin_gen
+from bitcoin.generator import G
 
 # we can verify that the generator point is indeed on the curve, i.e y^2 = x^3 + 7 (mod p)
-print("Generator IS on the curve : ", (Point.G.y**2 - Point.G.x**3 - 7) % Curve.bitcoin_curve.p == 0)
+print("Generator IS on the curve : ", (G.y**2 - G.x**3 - 7) % bitcoin_curve.p == 0)
 
 # some other totally random point will of course not be on the curve
 import random
 random.seed(1337)
-x = random.randrange(0, Curve.bitcoin_curve.p)
-y = random.randrange(0, Curve.bitcoin_curve.p)
-print("Totally random point is not: ", (y**2 - x**3 - 7) % Curve.bitcoin_curve.p == 0)
+x = random.randrange(0, bitcoin_curve.p)
+y = random.randrange(0, bitcoin_curve.p)
+print("Totally random point is not: ", (y**2 - x**3 - 7) % bitcoin_curve.p == 0)
+
+secret_key = int.from_bytes(b'Vinz Bitcoin Testnet', 'big')
+assert 1 <= secret_key < bitcoin_gen.n
+print(secret_key)
